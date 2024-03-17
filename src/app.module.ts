b5de666b824b40dcb3cc439de8ff233377dd5c3a
Robert Shymon <robisimon17@gmail.com>
2024-03-18 01:29:09 +0200

    Enables /users API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APIModule } from './modules/api/api.module';
 
 @Module({
-  imports: [],
+  imports: [APIModule],
   controllers: [AppController],
   providers: [AppService],
 })
