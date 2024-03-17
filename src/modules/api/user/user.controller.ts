351574258452df878141c6255e77b591421c9f81
Robert Shymon <robisimon17@gmail.com>
2024-03-18 01:31:49 +0200

    Installs swagger; adds a few routes to /users api

--- src/modules/api/user/user.controller.ts
+++ src/modules/api/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
 @Controller('users')
@@ -6,7 +7,20 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): string {
+  @ApiOperation({ description: 'Get all users' })
+  getUsers(): string {
+    return this.userService.getHello();
+  }
+
+  @Get()
+  @ApiOperation({ description: 'Get user by ID' })
+  getUser(): string {
+    return this.userService.getHello();
+  }
+
+  @Post()
+  @ApiOperation({ description: 'Register a new user' })
+  registerUser(): string {
     return this.userService.getHello();
   }
 }
